1086d4ff6eb7fc5bb226e9a682c5a77d7bb51c74
Roming22 <roming22@gmail.com>
2020-12-28 09:53:07 -0500

    Improve docker install

--- install/docker.sh
+++ install/docker.sh
@@ -4,6 +4,13 @@ SCRIPT_DIR="$(dirname $(realpath "${0}"))"
 source "$SCRIPT_DIR/env"
 
 docker(){
+    # https://docs.docker.com/engine/install/ubuntu/
+    apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
     add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     apt-get update -y
