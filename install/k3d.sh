f0f161d69fdd87040cefcc831c89f87ca1e7b987
Roming22 <roming22@gmail.com>
2020-12-27 19:13:27 -0500

    k3d: Create default admin-user and generate their token in ~/.kube

--- install/k3d.sh
+++ install/k3d.sh
@@ -13,6 +13,36 @@ configure(){
     # Use the latest image, as 1.18 does not support a reboot
     k3d cluster create "$USER" --image rancher/k3s:latest
     k3d kubeconfig merge "$USER" --switch-context
+
+    # Create default admin-user
+    cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+    cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+    # Generate a token
+    kubectl -n kubernetes-dashboard describe secret $(\
+        kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}' \
+    ) | grep -E "^token:" | sed "s/^token: *//" > ~/.kube/admin-user.token
+
+    # Display the current state
     kubectl get all --all-namespaces
 }
 export -f configure
