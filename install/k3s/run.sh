49a81606d6643e67a79cc33643c36b182cc57c62
Roming22 <roming22@gmail.com>
2020-12-30 12:28:18 -0500

    k3s: Retrieve a local copy of  from the cluster

--- install/k3s/run.sh
+++ install/k3s/run.sh
@@ -10,8 +10,12 @@ run(){
     ANSIBLE_DIR="${SCRIPT_DIR}/k3s-ansible"
     for CLUSTER in $(ls ${ANSIBLE_DIR}/inventory/ | grep -E -v "^sample$"); do
         echo; echo "[$CLUSTER]"
+        ALL_YML="${ANSIBLE_DIR}/inventory/${CLUSTER}/group_vars/all.yml"
         HOSTS_INI="${ANSIBLE_DIR}/inventory/${CLUSTER}/hosts.ini"
         ansible-playbook "${ANSIBLE_DIR}/site.yml" -i "${HOSTS_INI}"
+        DEFAULT_USER="$(grep -E "^ansible_user:" "${ALL_YML}" | sed -e "s/.*: //")"
+        MASTER0_IP="$(ansible-inventory --list -i "${HOSTS_INI}" | grep -A2 master | tail -1 | cut -d'"' -f2)"
+        scp "${DEFAULT_USER}@${MASTER0_IP}:~/.kube/config" "$HOME/.kube/config.${CLUSTER}"
         echo "[$CLUSTER: OK]"; echo
     done
 }
