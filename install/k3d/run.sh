49d9965f2c215ee231eaad66e9c862f4353d7520
Roming22 <roming22@gmail.com>
2021-03-09 11:37:59 -0500

    k3d: fix run.sh

--- install/k3d/run.sh
+++ install/k3d/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
+SCRIPT_DIR="$(dirname "$(realpath $0)")"
+ANSIBLE_DIR="${SCRIPT_DIR}/ansible"
 reset
 date
-ansible-playbook -v -i hosts.yml sites.yml
+ansible-playbook -v -i "${ANSIBLE_DIR}/hosts.yml" "${ANSIBLE_DIR}/sites.yml"
 scp pi@192.168.72.90:.kube/config /home/rarnaud/.kube/config
 cat ~/.kube/config
 kubectl get nodes
