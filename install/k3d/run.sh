74f7ccd002770457fddd20aed9fa998cf1850684
Roming22 <roming22@gmail.com>
2021-03-27 11:53:22 -0400

    Small improvements to run.sh

--- install/k3d/run.sh
+++ install/k3d/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 SCRIPT_DIR="$(dirname "$(realpath $0)")"
 ANSIBLE_DIR="${SCRIPT_DIR}/ansible"
+TARGET="192.168.72.90"
 reset
 date
 ansible-playbook -v -i "${ANSIBLE_DIR}/hosts.yml" "${ANSIBLE_DIR}/sites.yml"
-scp pi@192.168.72.90:.kube/config /home/rarnaud/.kube/config
+scp "${TARGET}:.kube/config" /home/rarnaud/.kube/config
 cat ~/.kube/config
 kubectl get nodes
-curl -k -s https://192.168.72.90/
+curl -k -s "https://${TARGET}/"
 date
